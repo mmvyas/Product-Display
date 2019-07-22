b8f5f30a88950af18d4bd4e929917988f1bb2bd1
mmvyas <mansivyas39@gmail.com>
2019-07-22 17:48:43 +1000

    Removed Radiobutton added Dropdown

--- product_display/src/App.js
+++ product_display/src/App.js
@@ -1,30 +1,59 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import './App.css';
-
 //Import of json information to build page
 import products from './products.json';
-import type from './type.json';
 
 //Import of custom Components
-import Filter from './filter/Filter';
 import Products from './products/Products';
 
 export default class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      selectedCategory: props.type
+      selectedCategory: props.type,
+      options: [
+        {
+          name: 'All',
+          value: '',
+        },
+       
+        {
+          name: 'Beer',
+          value: 'Beer',
+        },
+        {
+          name: 'Wine',
+          value: 'Wine',
+        },
+        {
+          name: 'Cidder',
+          value: 'Cider',
+        },
+        {
+          name: 'Spirits',
+          value: 'Spirits',
+        },
+      ],
+      
     }
   }
 
+  
+
   handleChange = filter => {
-    this.setState({
-      selectedCategory: filter.target.value
-    });
-  }
+    this.setState({ selectedCategory: filter.target.value });
+  };
+
+  
 
   render() {
+    const { options, value } = this.state;
+    let dStyle={
+      color: 'rgba(0, 0, 0, 0.54)',
+      fontSize: '1em'
+    };
     return (
+      
       <div className="App">
         <div className="App-content">
           <h3 className="Page-header">
@@ -32,12 +61,23 @@ export default class App extends Component {
           </h3>
           <p className="Page-Description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
+        
         <div className="App-sidebar">
-          <Filter type={type.type} onFilter={this.handleChange}/>
-        </div>
+        <h3>Filter Products {value}</h3>
+        <div className="Dropdown" >
+        <select onChange={this.handleChange} style = {dStyle} value={value} onFilter={this.props.onFilter} selected={true}>
+          {options.map(item => (
+            <option key={item.value} value={item.value}>
+              {item.name}
+            </option>
+          ))}
+        </select> </div> </div>
+           
+        
         <div className="App-content-products">
           <Products products={products.products} filter={this.state.selectedCategory}/>
         </div>
+        
       </div>
     );
   }
